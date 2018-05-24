cc4897b51b175c3513e57a724dd23f1c60eb8568
Melwin Magalhaes <melwin.magalhaes@gmail.com>
2018-05-24 11:39:57 +0200

    Add inactive layout for StepTab

--- ctksteppager/src/main/java/com/chronotruck/ctksteppager/CtkStepTab.kt
+++ ctksteppager/src/main/java/com/chronotruck/ctksteppager/CtkStepTab.kt
@@ -2,6 +2,8 @@ package com.chronotruck.ctksteppager
 
 import android.content.Context
 import android.support.constraint.ConstraintLayout
+import android.support.constraint.ConstraintSet
+import android.transition.TransitionManager
 import android.util.AttributeSet
 
 /**
@@ -14,7 +16,32 @@ class CtkStepTab @JvmOverloads constructor(
         attrs: AttributeSet? = null,
         defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    private val constraintSetActive = ConstraintSet()
+    private val constraintSetInactive = ConstraintSet()
+    private var constraintLayout: ConstraintLayout = inflate(context, R.layout.layout_ctk_step_tab_active, this) as ConstraintLayout
+
     init {
-        inflate(context, R.layout.layout_ctk_step_tab, this)
+        initConstraintLayout()
+        switchLayout(isActivated)
+    }
+
+    override fun setActivated(activated: Boolean) {
+        super.setActivated(activated)
+        switchLayout(activated)
+    }
+
+    private fun initConstraintLayout() {
+        constraintSetActive.clone(constraintLayout)
+        constraintSetInactive.clone(context, R.layout.layout_ctk_step_tab_inactive)
+    }
+
+    private fun switchLayout(activated: Boolean) {
+        TransitionManager.beginDelayedTransition(constraintLayout)
+        if (activated) {
+            constraintSetActive.applyTo(constraintLayout)
+        } else {
+            constraintSetInactive.applyTo(constraintLayout)
+        }
     }
 }
--- sample/src/main/java/com/chronotruck/ctksteppager/MainActivity.kt
+++ sample/src/main/java/com/chronotruck/ctksteppager/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.chronotruck.ctksteppager
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        switch_button.setOnClickListener {
+            switchStepBarState(!ctkStepTab.isActivated)
+        }
+    }
+
+    private fun switchStepBarState(activated: Boolean) {
+        ctkStepTab.isActivated = activated
     }
 }
