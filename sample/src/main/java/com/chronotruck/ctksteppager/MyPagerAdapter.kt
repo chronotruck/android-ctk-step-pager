d780477cdc8dbb57cdb24b7e0cdbe58c134483e4
Melwin Magalhaes <melwin.magalhaes@gmail.com>
2018-05-28 15:54:53 +0200

    Update sample

--- sample/src/main/java/com/chronotruck/ctksteppager/MyPagerAdapter.kt
+++ sample/src/main/java/com/chronotruck/ctksteppager/MyPagerAdapter.kt
@@ -1,9 +1,27 @@
 package com.chronotruck.ctksteppager
 
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+
 /**
  * @Author McGalanes
  * @Email melwin.magalhaes@gmail.com
  * @Project android-ctk-step-pager
  */
-class PagerAdapter {
+class MyPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
+
+    override fun getItem(position: Int): Fragment = MyStepFragment.newInstance(position)
+
+    override fun getCount(): Int = 4
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when (position) {
+            0 -> "One"
+            1 -> "Two"
+            2 -> "Three"
+            3 -> "Four"
+            else -> throw IllegalArgumentException("No page title found for position $position")
+        }
+    }
 }
