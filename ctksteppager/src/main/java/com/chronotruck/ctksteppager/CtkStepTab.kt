0f85dcf411ffa24643b230bb9cdfcaab793927c0
Melwin Magalhaes <melwin.magalhaes@gmail.com>
2018-05-28 10:50:36 +0200

    Add hide/show label animation on expand

--- ctksteppager/src/main/java/com/chronotruck/ctksteppager/CtkStepTab.kt
+++ ctksteppager/src/main/java/com/chronotruck/ctksteppager/CtkStepTab.kt
@@ -6,6 +6,8 @@ import android.content.Context
 import android.support.constraint.ConstraintLayout
 import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
+import android.view.View
+import android.view.animation.DecelerateInterpolator
 import android.widget.LinearLayout
 import android.widget.TextView
 
@@ -20,13 +22,15 @@ class CtkStepTab @JvmOverloads constructor(
         defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
 
-    val titleTv: TextView
+    private val titleTv: TextView
+    private val badgeTv: TextView
 
     val isExpanded get() = isActivated
 
     init {
         inflate(context, R.layout.layout_ctk_step_tab, this)
         titleTv = findViewById(R.id.title_textview)
+        badgeTv = findViewById(R.id.badge_textview)
     }
 
     override fun onAttachedToWindow() {
@@ -47,7 +51,7 @@ class CtkStepTab @JvmOverloads constructor(
         isActivated = true
 
         ValueAnimator.ofInt(measuredWidth, Util.getDeviceScreenSize(context).x).apply {
-            duration = 1000
+            interpolator = DecelerateInterpolator()
             addUpdateListener {
                 layoutParams.width = it.animatedValue as Int
                 requestLayout()
@@ -57,8 +61,6 @@ class CtkStepTab @JvmOverloads constructor(
                 }
 
                 override fun onAnimationEnd(p0: Animator?) {
-                    titleTv.visibility = VISIBLE
-
                     (layoutParams as LinearLayout.LayoutParams).apply {
                         width = LinearLayout.LayoutParams.MATCH_PARENT
                         weight = 1f
@@ -69,7 +71,58 @@ class CtkStepTab @JvmOverloads constructor(
                 }
 
                 override fun onAnimationStart(p0: Animator?) {
+                    hideLabels { showLabels() }
                     setBackgroundColor(ContextCompat.getColor(context, R.color.blue))
+                }
+            })
+        }.start()
+    }
+
+    private fun hideLabels(onAnimationEnd: () -> Unit) {
+        ValueAnimator.ofFloat(1f, 0f).apply {
+            duration = 0
+            addUpdateListener {
+                titleTv.alpha = it.animatedValue as Float
+                badgeTv.alpha = it.animatedValue as Float
+            }
+            addListener(object : Animator.AnimatorListener {
+                override fun onAnimationRepeat(p0: Animator?) {
+                }
+
+                override fun onAnimationEnd(p0: Animator?) {
+                    titleTv.visibility = View.GONE
+                    badgeTv.visibility = View.GONE
+                    onAnimationEnd.invoke()
+                }
+
+                override fun onAnimationCancel(p0: Animator?) {
+                }
+
+                override fun onAnimationStart(p0: Animator?) {
+                }
+            })
+        }.start()
+    }
+
+    private fun showLabels() {
+        ValueAnimator.ofFloat(0f, 1f).apply {
+            addUpdateListener {
+                titleTv.alpha = it.animatedValue as Float
+                badgeTv.alpha = it.animatedValue as Float
+            }
+            addListener(object : Animator.AnimatorListener {
+                override fun onAnimationRepeat(p0: Animator?) {
+                }
+
+                override fun onAnimationEnd(p0: Animator?) {
+                }
+
+                override fun onAnimationCancel(p0: Animator?) {
+                }
+
+                override fun onAnimationStart(p0: Animator?) {
+                    titleTv.visibility = View.VISIBLE
+                    badgeTv.visibility = View.VISIBLE
                 }
             })
         }.start()
