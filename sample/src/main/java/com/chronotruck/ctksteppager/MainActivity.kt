dd96538fa9657d3086fc4fc05de9eef5215a69bf
Melwin Magalhaes <melwin.magalhaes@gmail.com>
2018-05-29 14:42:56 +0200

    Update sample

--- sample/src/main/java/com/chronotruck/ctksteppager/MainActivity.kt
+++ sample/src/main/java/com/chronotruck/ctksteppager/MainActivity.kt
@@ -2,22 +2,25 @@ package com.chronotruck.ctksteppager
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        init()
-    }
-
-    private fun init() {
         steppager.adapter = MyPagerAdapter(supportFragmentManager)
 
-        done_button.setOnClickListener {
-            steppager.doneCurrentStepTab()
+        done_button.setOnClickListener(this)
+        reset_button.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when (view!!.id) {
+            R.id.done_button -> steppager.doneCurrentStepTab()
+            R.id.reset_button -> steppager.resetAllStepTabs()
         }
     }
 }
